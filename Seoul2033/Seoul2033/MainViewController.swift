046d1ccf5cd7f6f92e5c90009131f67dc8e91d5e
ChoiSeoyeon61 <63771194+ChoiSeoyeon61@users.noreply.github.com>
2020-09-21 21:49:03 +0900

    테이블뷰 고침, 체력/멘탈/돈 변경 기능 추가

--- Seoul2033/Seoul2033/MainViewController.swift
+++ Seoul2033/Seoul2033/MainViewController.swift
@@ -10,62 +10,38 @@ import UIKit
 
 class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate {
  
-     func numberOfSections(in tableView: UITableView) -> Int {
+    
+        //섹션 : 1개
+        func numberOfSections(in tableView: UITableView) -> Int {
             return 1
          }
-         //섹션 속 셀 개수 : Choice 어레이 안 Choice 개수
+         //섹션 속 셀 개수 : Choice 개수
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             
-            let currentChoice = currentPage.choice.count
+            let currentChoice = santa.gameCharacter.currentPage().choice.count
             print(currentChoice)
             return currentChoice
         }
            
-           //각 셀 별 내용
+        //각 셀 별 내용 : 텍스트 띄우기
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
             
             let cell = tableView.dequeueReusableCell(withIdentifier: "choiceCell") as! ChoiceTableViewCell
             let list: Choice
-            list = currentPage.choice[indexPath.row]
+            list = santa.gameCharacter.currentPage().choice[indexPath.row]
             cell.choiceLable.text = list.choiceText
         
             
             return cell
         }
-        
-      func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    
+//셀 높이
+        func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
             return CGFloat(55)
-         }
-        
-        // 셀 선택되었을 때 행동
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            
-        //캐릭터 현재 페이지 위치를 choice nextPageIndex 이용해서 업데이트함. 1 뺀 이유는 실제 인덱스값이랑 currentPageNumber가 1 차이 나서.
-            santaCharacter1.currentPageNumber = currentPage.choice[indexPath.row].nextPageIndex
-        // 페이지 storytext를 위 값 이용해 업뎃
-            testLable.text = "\(truckSaleEP[santaCharacter1.currentPageNumber].storyText)"
-        // 선택지 테이블 뷰 새로고침(이나.. 현재까지 작동 안 되는 중)
-      
-            self.choiceTableView.reloadData()
-         
-          
-            
-        
-        // 캐릭터
-    //        if prologueEP[santaCharacter1.currentPageNumber].choice.count != 0 {
-    //            if prologueEP[santaCharacter1.currentPageNumber].choice[indexPath.row].abilityGive == true {
-    //                santaCharacter1.ability += prologueEP[santaCharacter1.currentPageNumber].choice[indexPath.row].ability
-    //            } else {
-    //                // abilityGive가 false일 때 캐릭터 ability랑 choice ability랑 비교해서 공통인 부분을 캐릭터 ability에서 삭제해야 함.
-    //                for i in santaCharacter1.ability{
-    //
-    //                }
-    //            }
-    //
-    //
-    //        }
         }
-// 테스트레이블, 체력등 이미지, 페이지 넘버...
+
+   
+// 아웃렛
     @IBOutlet weak var testLable: UILabel!
     
     @IBOutlet weak var healthImage: UIImageView!
@@ -75,49 +51,105 @@ class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegat
     
     @IBOutlet weak var PagescrollView: UIScrollView!
     @IBOutlet weak var choiceTableView: UITableView!
+    @IBOutlet weak var abilityLable: UILabel!
     
+    @IBOutlet weak var testh: UILabel!
     // 뷰디드로드
-   override func viewDidLoad() {
-         super.viewDidLoad()
-       self.choiceTableView.dataSource = self
-       self.choiceTableView.delegate = self
+    @IBOutlet weak var textM: UILabel!
+    @IBOutlet weak var testMm: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.choiceTableView.dataSource = self
+        self.choiceTableView.delegate = self
     
-    // 첫 시작 stroyText.
+        testLable.text = santa.gameCharacter.currentPage().storyText
+        pageNumber.text = "\(santa.gameCharacter.pageIndex)"
+        abilityLable.text = "\(santa.gameCharacter.ability)"
     
-    
-    // !!!!!!!!!!!현재 "페이지" 위치!!!!!! / 그런데 이 값이 업데이트가 안 되는 것 같음.. 이유는 santaCharacter1.currentPageNumbe가 반영이 안 되고 있는 것 같은데 이유를 모르겠음.
-//    var currentPage = prologueEP[santaCharacter1.currentPageNumber-1]
-    
-
-    
-    // 섹션 수 : 1
-   
-    // 셀 비활성화
-     
-
-   testLable.text = currentPage.storyText
-        
+        healthImage.image = UIImage(named: "health3")
+        mentalImage.image = UIImage(named: "mental3")
+        moneyImage.image = UIImage(named: "money3")
     }
    
    
-    var currentPage = truckSaleEP[santaCharacter1.currentPageNumber]
+   
     
     
-    func changeCharecterAbility() {
+    func changeCharacterAbility() {
        
        
     }
 
-    func changeCharcterHp() {
+    func changeHpMentalMoney() {
         
     }
     
     func onOffChoiceCell() {
           
     }
+    // 페이지 업데이트
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       
+        // 체력 / 멘탈 / 돈 데이터 업뎃
+        santa.gameCharacter.health += santa.gameCharacter.currentPage().choice[indexPath.row].health
+        santa.gameCharacter.mental += santa.gameCharacter.currentPage().choice[indexPath.row].mental
+        santa.gameCharacter.money += santa.gameCharacter.currentPage().choice[indexPath.row].money
+        
+        // 능력 주기 or 뺏기
+        if santa.gameCharacter.currentPage().choice[indexPath.row].ability != [] {
+            if santa.gameCharacter.currentPage().choice[indexPath.row].abilityGive == true {
+                santa.gameCharacter.ability += santa.gameCharacter.currentPage().choice[indexPath.row].ability
+            } else {
+                for lable in santa.gameCharacter.ability.enumerated() {
+                    if lable.element == santa.gameCharacter.currentPage().choice[indexPath.row].ability[0] {
+                        santa.gameCharacter.ability.remove(at: lable.offset)
+                    }
+                }
+            }
+               
+        }
+        
+        // 체력 / 멘탈 / 돈 이미지 업뎃
+        healthImage.image = UIImage(named: "health\(santa.gameCharacter.health)")
+        mentalImage.image = UIImage(named: "mental\( santa.gameCharacter.mental)")
+        moneyImage.image = UIImage(named: "money\(santa.gameCharacter.money)")
+        santa.gameCharacter.pageIndex += 1
+       
+        //현 에피소드의 페이지 인덱스값
+        santa.gameCharacter.currentEpPageIndex = santa.gameCharacter.currentPage().choice[indexPath.row].nextPageIndex
+        
+        // 페이지 storytext를 위 값 이용해 업뎃
+        testLable.text = "\(santa.gameCharacter.currentPage().storyText)"
+        pageNumber.text = "\(santa.gameCharacter.pageIndex)"
+        abilityLable.text = "\(santa.gameCharacter.ability)"
+        
+       
+       
+       
+        
+        //tableView 업뎃
+        self.choiceTableView.reloadData()
+     
+      
+       
+        
+        
+        }
     
-   
-    
+    // 캐릭터
+//        if prologueEP[santaCharacter1.currentPageNumber].choice.count != 0 {
+//            if prologueEP[santaCharacter1.currentPageNumber].choice[indexPath.row].abilityGive == true {
+//                santaCharacter1.ability += prologueEP[santaCharacter1.currentPageNumber].choice[indexPath.row].ability
+//            } else {
+//                // abilityGive가 false일 때 캐릭터 ability랑 choice ability랑 비교해서 공통인 부분을 캐릭터 ability에서 삭제해야 함.
+//                for i in santaCharacter1.ability{
+//
+//                }
+//            }
+//
+//
+//        }
+        
   
 
     
