6d981a312a3e3a84807ceb22c88338dbf09c59ed
ChoiSeoyeon61 <63771194+ChoiSeoyeon61@users.noreply.github.com>
2020-09-24 09:14:30 +0900

    none

--- ttestt/ttestt/ViewController.swift
+++ ttestt/ttestt/ViewController.swift
@@ -8,12 +8,17 @@
 
 import UIKit
 import RealityKit
+import ARKit
 
 class ViewController: UIViewController {
     
     @IBOutlet var arView: ARView!
     
 //  .notifications.goToDae.post()
+    func handleTapOnEntity(_ entity: Entity?) {
+           guard let entity = entity else { return }
+        print("ready")
+       }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
@@ -25,10 +30,8 @@ class ViewController: UIViewController {
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
         
-        if (try! ChangdeokAR.loadBoxxDae().actions.goToDae.onAction != nil) {
-//            performSegue(withIdentifier: "goToVC", sender: nil)
-            print("ready")
-        }
+        try! ChangdeokAR.loadBoxxDae().actions.goToDae.onAction = handleTapOnEntity(_:)
+//
             
         
         }
